c2d1d6a2360ba9f6e14327a147f52cef9ce276c5
Hyunchul <chc3954@gmail.com>
2024-10-24 23:42:47 -0400

    hashing password

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -16,16 +16,17 @@ export class UsersService {
 
   async createAccount(
     createAccountInput: CreateAccountInput,
-  ): Promise<string | undefined> {
+  ): Promise<{ ok: boolean; error?: string }> {
     const { email, password, role } = createAccountInput;
     try {
       const exists = await this.users.findOne({ where: { email } });
       if (exists) {
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return 'Could not create account.';
+      return { ok: false, error: 'Could not create account.' };
     }
   }
 }
