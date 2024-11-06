da57922d163ac18df33f005d4d0763c736288f1d
Hyunchul Cho <chc3954@gmail.com>
2024-11-06 15:20:54 -0500

    createAccount e2e testing

--- server/test/users.e2e-spec.ts
+++ server/test/users.e2e-spec.ts
@@ -3,38 +3,90 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { DataSource } from 'typeorm';
+import exp from 'constants';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    dataSource = module.get<DataSource>(DataSource);
     app = module.createNestApplication();
     await app.init();
   });
 
   afterAll(async () => {
-    const dataSource: DataSource = new DataSource({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    });
-    const connection: DataSource = await dataSource.initialize();
-    await connection.dropDatabase();
-    await connection.destroy();
-    app.close();
+    await dataSource.dropDatabase();
+    await dataSource.destroy();
+    await app.close();
   });
 
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL = 'test@email.com';
+    it('should create new account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "1234",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(input: {
+              email: "${EMAIL}",
+              password: "1234",
+              role: Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
+  });
+
+  describe('login', () => {});
   it.todo('userProfile');
-  it.todo('editProfile');
+  it.todo('me');
   it.todo('verifyEmail');
-  it.todo('createAccount');
-  it.todo('login');
+  it.todo('editProfile');
 });
