43dcfeefd20a03144ec420f28e376f174c8ba579
Hyunchul Cho <chc3954@gmail.com>
2024-11-24 15:58:11 -0500

    Implement getOrders, getOrder

--- server/src/orders/orders.resolver.ts
+++ server/src/orders/orders.resolver.ts
@@ -1,10 +1,12 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { Order } from './entities/order.entity';
 import { OrdersService } from './orders.service';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Role } from 'src/auth/role.decorator';
 import { AuthUser } from 'src/auth/auth.decorator';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 
 @Resolver((of) => Order)
 export class OrdersResolver {
@@ -17,5 +19,23 @@ export class OrdersResolver {
     @Args('input') createOrderInput: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     return this.ordersService.createOrder(customer, createOrderInput);
+  }
+
+  @Query((returns) => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @AuthUser() user: User,
+    @Args('input') getOrdersInput: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    return this.ordersService.getOrders(user, getOrdersInput);
+  }
+
+  @Query((returns) => GetOrderOutput)
+  @Role(['Any'])
+  async getOrder(
+    @AuthUser() user: User,
+    @Args('input') getOrderInput: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    return this.ordersService.getOrder(user, getOrderInput);
   }
 }
--- server/src/orders/orders.service.ts
+++ server/src/orders/orders.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { Repository } from 'typeorm';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
+import { GetOrderInput, GetOrderOutput } from './dtos/get-order.dto';
 
 @Injectable()
 export class OrdersService {
@@ -26,7 +28,7 @@ export class OrdersService {
     { restaurantId, items }: CreateOrderInput,
   ): Promise<CreateOrderOutput> {
     try {
-      const restaurant = await this.restaurants.findOneOrFail({
+      const restaurant = await this.restaurants.findOne({
         where: { id: restaurantId },
       });
       if (!restaurant) {
@@ -39,7 +41,7 @@ export class OrdersService {
       let orderTotal = 0;
       const orderItems: OrderItem[] = [];
       for (const item of items) {
-        const dish = await this.dishes.findOneOrFail({
+        const dish = await this.dishes.findOne({
           where: { id: item.dishId },
         });
         if (!dish) {
@@ -83,6 +85,90 @@ export class OrdersService {
       return { ok: true };
     } catch {
       return { ok: false, error: 'Could not create order.' };
+    }
+  }
+
+  async getOrders(
+    user: User,
+    { status }: GetOrdersInput,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+
+      switch (user.role) {
+        case UserRole.Client:
+          orders = await this.orders.find({
+            where: { customer: { id: user.id }, ...(status && { status }) },
+          });
+          break;
+        case UserRole.Delivery:
+          orders = await this.orders.find({
+            where: { driver: { id: user.id }, ...(status && { status }) },
+          });
+          break;
+        case UserRole.Owner:
+          const restaurants = await this.restaurants.find({
+            where: { owner: { id: user.id } },
+            relations: ['orders'],
+          });
+          orders = restaurants
+            .map((restaurant) => restaurant.orders)
+            .flat(1)
+            .filter((order) => (status ? order.status === status : true));
+          break;
+      }
+      return { ok: true, orders };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not get orders.',
+      };
+    }
+  }
+
+  canSeeOrder = (user: User, order: Order): boolean => {
+    let canSee: boolean = true;
+    switch (user.role) {
+      case UserRole.Client:
+        canSee = order.customerId === user.id;
+        break;
+      case UserRole.Delivery:
+        canSee = order.driverId === user.id;
+        break;
+      case UserRole.Owner:
+        canSee = order.restaurant.ownerId === user.id;
+        break;
+    }
+    return canSee;
+  };
+
+  async getOrder(
+    user: User,
+    { id: orderId }: GetOrderInput,
+  ): Promise<GetOrderOutput> {
+    try {
+      const order = await this.orders.findOne({ where: { id: orderId } });
+      if (!order) {
+        return {
+          ok: false,
+          error: 'Order not found.',
+        };
+      }
+      if (!this.canSeeOrder(user, order)) {
+        return {
+          ok: false,
+          error: 'You cannot see that.',
+        };
+      }
+      return {
+        ok: true,
+        order,
+      };
+    } catch {
+      return {
+        ok: false,
+        error: 'Could not get order.',
+      };
     }
   }
 }
