4f7a487cd43737c027757f7be1787c078e0af365
Hyunchul Cho <chc3954@gmail.com>
2024-12-29 13:48:06 -0500

    Upgrade search bar and Move it to the header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,11 +2,30 @@ import React from "react";
 import logo from "../images/uber-eats.svg";
 import { useMe } from "../hooks/useMe";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faCircleXmark, faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface ISearchForm {
+  searchTerm: string;
+}
 
 export const Header: React.FC = () => {
   const { data } = useMe();
+  const navigate = useNavigate();
+  const { register, handleSubmit, getValues, watch, setValue } = useForm<ISearchForm>();
+
+  const onSearch = () => {
+    const { searchTerm } = getValues();
+    navigate({
+      pathname: "/search",
+      search: `?term=${searchTerm}`,
+    });
+  };
+
+  const onClearSearch = () => {
+    setValue("searchTerm", "");
+  };
 
   return (
     <>
@@ -15,16 +34,40 @@ export const Header: React.FC = () => {
           <span>Please verify your email</span>
         </div>
       )}
-      <header className="py-4 px-4 lg:px-8">
+      <header className="py-2 px-4 lg:px-8">
         <div className="w-full max-w-screen bg-white mx-auto flex justify-between items-center">
           <Link to="/">
             <img src={logo} className="w-24" />
           </Link>
-          <span className="text-sm">
-            <Link to="/my-profile">
-              <FontAwesomeIcon icon={faUser} /> {data?.me.email}
+          <form className="w-2/3 max-w-screen-sm text-md" onSubmit={handleSubmit(onSearch)}>
+            <div className="relative flex items-center">
+              <input
+                {...register("searchTerm", { required: true, min: 3 })}
+                type="search"
+                className="w-full pl-12 py-2 rounded-full bg-gray-200 border-2 border-white font-light focus:outline-none focus:border-gray-500 transition-colors"
+                placeholder="Search restaurants"
+              />
+              <span className="absolute left-4">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </span>
+              {watch("searchTerm") && (
+                <span className="absolute right-4">
+                  <button
+                    type="button"
+                    className="text-md text-gray-600 rounded-full"
+                    onClick={onClearSearch}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                  </button>
+                </span>
+              )}
+            </div>
+          </form>
+          <div className="text-sm">
+            <Link to="/my-profile" className="flex items-center justify-center">
+              <div className="mr-2 hidden lg:inline-block">{data?.me.email}</div>
+              <FontAwesomeIcon icon={faUser} className="p-2 block bg-gray-200 rounded-full" />
             </Link>
-          </span>
+          </div>
         </div>
       </header>
     </>
--- client/src/pages/client/home.tsx
+++ client/src/pages/client/home.tsx
@@ -5,13 +5,8 @@ import { Helmet } from "react-helmet-async";
 import { Restaurant } from "../../components/restaurant";
 import { Categories } from "../../components/categories";
 import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import { CATEGORY_FRAGMENT, RESTAURANT_FRAGMENT } from "../../fragments";
 import { Restaurants } from "../../components/restaurants";
-
-interface ISearchForm {
-  searchTerm: string;
-}
 
 const RESTAURANTS_QUERY = gql`
   query restaurants($input: RestaurantsInput!) {
@@ -37,40 +32,18 @@ const RESTAURANTS_QUERY = gql`
 `;
 
 export const Home = () => {
-  const navigate = useNavigate();
   const [page, setPage] = useState(1);
   const { data, loading, error } = useQuery<RestaurantsQuery, RestaurantsQueryVariables>(
     RESTAURANTS_QUERY,
     { variables: { input: { page: page } } }
   );
-  const { register, handleSubmit, getValues } = useForm<ISearchForm>();
-
-  const onPrevPageClick = () => setPage((current) => current - 1);
-  const onNextPageClick = () => setPage((current) => current + 1);
-
-  const onSearch = () => {
-    const { searchTerm } = getValues();
-    navigate({
-      pathname: "/search",
-      search: `?term=${searchTerm}`,
-    });
-  };
 
   return (
     <div>
       <Helmet>
         <title>Home | Yuber Eats</title>
       </Helmet>
-      <div className="bg-gray-800 w-full py-20 flex items-center justify-center">
-        <form onSubmit={handleSubmit(onSearch)}>
-          <input
-            {...register("searchTerm", { required: true, min: 3 })}
-            type="search"
-            className="input rounded-md border-0 w-1/2 min-w-80"
-            placeholder="Search restaurants"
-          />
-        </form>
-      </div>
+      <div className="bg-gray-800 w-full py-20 flex items-center justify-center"></div>
       {!loading && (
         <div className="container">
           <div className="flex justify-around max-w-screen-sm mx-auto">
