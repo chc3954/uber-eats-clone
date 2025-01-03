f8a1195272f916c7c69c6e56610693bd7da9ef34
Hyunchul Cho <chc3954@gmail.com>
2025-01-02 21:32:42 -0500

    Working on adding options during adding dish

--- client/src/pages/owner/add-dish.tsx
+++ client/src/pages/owner/add-dish.tsx
@@ -1,16 +1,20 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { CreateDishMutation, CreateDishMutationVariables } from "../../__generated__/graphql";
 import { useForm } from "react-hook-form";
 import { Helmet } from "react-helmet-async";
 import { Button } from "../../components/button";
 import { MY_RESTAURANT_QUERY } from "./my-restaurant";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { v4 as uuidv4 } from "uuid";
 
 interface IFromProps {
   name: string;
   price: string;
   description: string;
+  [options: string]: string;
 }
 
 const CREATE_DISH_MUTATION = gql`
@@ -24,11 +28,14 @@ const CREATE_DISH_MUTATION = gql`
 
 export const AddDishPage = () => {
   const navigate = useNavigate();
+  const [optionsNumber, setOptionsNumber] = useState<number[]>([]);
   const { id: restaurantId } = useParams() as { id: string };
   const {
+    control,
     register,
     handleSubmit,
     getValues,
+    setValue,
     formState: { isValid },
   } = useForm<IFromProps>({
     mode: "onChange",
@@ -41,7 +48,7 @@ export const AddDishPage = () => {
   );
 
   const onAddDish = () => {
-    const { name, price, description } = getValues();
+    const { name, price, description, ...options } = getValues();
     createDish({
       variables: {
         createDishInput: {
@@ -53,6 +60,16 @@ export const AddDishPage = () => {
       },
     });
     navigate(-1);
+  };
+
+  const onAddOption = () => {
+    setOptionsNumber((current) => [...current, +uuidv4()]);
+  };
+
+  const onDeleteOption = (optionId: number) => {
+    setValue(`optionName-${optionId}`, "");
+    setValue(`optionExtra-${optionId}`, "");
+    setOptionsNumber((current) => current.filter((id) => id !== optionId));
   };
 
   return (
@@ -83,6 +100,37 @@ export const AddDishPage = () => {
           type="text"
           placeholder="Description"
         />
+        <div className="my-10">
+          <h4 className="font-medium mb-3 text-lg">Dish Option</h4>
+          <span
+            className="cursor-pointer text-white bg-gray-900 py-1 px-2 mt-5"
+            onClick={onAddOption}>
+            Add Dish Option
+          </span>
+          {optionsNumber.length > 0 &&
+            optionsNumber.map((id) => (
+              <div key={id} className="mt-5">
+                <input
+                  {...register(`optionName-${id}`)}
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2 mr-3"
+                  type="text"
+                  placeholder="Option Name"
+                />
+                <input
+                  {...register(`optionExtra-${id}`, { valueAsNumber: true })}
+                  className="py-2 px-4 focus:outline-none focus:border-gray-600 border-2"
+                  type="number"
+                  min={0}
+                  placeholder="Option Extra"
+                />
+                <span
+                  className="cursor-pointer text-gray-500 hover:text-red-400 py-3 px-4"
+                  onClick={() => onDeleteOption(id)}>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+                </span>
+              </div>
+            ))}
+        </div>
         <Button loading={loading} disable={!isValid} actionText="Create Dish" />
       </form>
     </div>
